f262dc20e71d3730e3421a539c4206466b706bb4
Barak <Barakraz6@gmail.com>
2019-05-11 23:57:52 +0300

    Convert main test InitializeDishesTest to separate InitializeDishTest by each Dish type
     - New test to Initialize Appetizer and MainDish from all types

---
+++ RestaurantApplication/RestaurantMSTest/InitializeDishTests/InitializeAppetizerTests.cs
@@ -0,0 +1,71 @@
+﻿using DishCreator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantApplication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static RestaurantApplication.DishTypes;
+
+namespace RestaurantMSTest.InitializeDishTests
+{
+    [TestClass]
+    public class InitializeAppetizerTests
+    {
+        [TestMethod]
+        public void InitializeAppetizer_ChickenNuggets()
+        {
+            string name = "Chicken Nuggets";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eAppetizer appetizerType = eAppetizer.ChickenNuggets;
+            Appetizer appetizer = InitializeDishes.InitializeAppetizer(name, quantity, dinerSerialNumber, appetizerType);
+
+            Assert.AreEqual(appetizerType, appetizer.AppetizerType);
+            Assert.AreEqual(dinerSerialNumber, appetizer.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.Appetizer], appetizer.MainRank);
+            Assert.AreEqual(name, appetizer.Name);
+            Assert.AreEqual(DishPrices.AppetizerPrices[appetizerType], appetizer.Price);
+            Assert.AreEqual(quantity, appetizer.Quantity);
+            Assert.AreEqual(1, appetizer.SecondRank);
+            Assert.AreEqual(DishRanks.AppetizerRanks[appetizerType], appetizer.ThirdRank);
+        }
+
+        [TestMethod]
+        public void InitializeAppetizer_ChickenSkewers()
+        {
+            string name = "Chicken Skewers";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eAppetizer appetizerType = eAppetizer.ChickenSkewers;
+            Appetizer appetizer = InitializeDishes.InitializeAppetizer(name, quantity, dinerSerialNumber, appetizerType);
+
+            Assert.AreEqual(appetizerType, appetizer.AppetizerType);
+            Assert.AreEqual(dinerSerialNumber, appetizer.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.Appetizer], appetizer.MainRank);
+            Assert.AreEqual(name, appetizer.Name);
+            Assert.AreEqual(DishPrices.AppetizerPrices[appetizerType], appetizer.Price);
+            Assert.AreEqual(quantity, appetizer.Quantity);
+            Assert.AreEqual(1, appetizer.SecondRank);
+            Assert.AreEqual(DishRanks.AppetizerRanks[appetizerType], appetizer.ThirdRank);
+        }
+
+        [TestMethod]
+        public void InitializeAppetizer_ChickenWings()
+        {
+            string name = "Chicken Wings";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eAppetizer appetizerType = eAppetizer.ChickenWings;
+            Appetizer appetizer = InitializeDishes.InitializeAppetizer(name, quantity, dinerSerialNumber, appetizerType);
+
+            Assert.AreEqual(appetizerType, appetizer.AppetizerType);
+            Assert.AreEqual(dinerSerialNumber, appetizer.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.Appetizer], appetizer.MainRank);
+            Assert.AreEqual(name, appetizer.Name);
+            Assert.AreEqual(DishPrices.AppetizerPrices[appetizerType], appetizer.Price);
+            Assert.AreEqual(quantity, appetizer.Quantity);
+            Assert.AreEqual(1, appetizer.SecondRank);
+            Assert.AreEqual(DishRanks.AppetizerRanks[appetizerType], appetizer.ThirdRank);
+        }        
+    }
+}
---
+++ RestaurantApplication/RestaurantMSTest/InitializeDishTests/InitializeMainDishTests.cs
@@ -0,0 +1,71 @@
+﻿using DishCreator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantApplication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static RestaurantApplication.DishTypes;
+
+namespace RestaurantMSTest.InitializeDishTests
+{
+    [TestClass]
+    public class InitializeMainDishTests
+    {
+        [TestMethod]
+        public void InitializeMainDish_ChickenBreast()
+        {
+            string name = "Chicken Breast";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eMainDish mainDishType = eMainDish.ChickenBreast;
+            MainDish mainDish = InitializeDishes.InitializeMainDish(name, quantity, dinerSerialNumber, mainDishType);
+
+            Assert.AreEqual(mainDishType, mainDish.MainDishType);
+            Assert.AreEqual(dinerSerialNumber, mainDish.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.MainDish], mainDish.MainRank);
+            Assert.AreEqual(name, mainDish.Name);
+            Assert.AreEqual(DishPrices.MainDishPrices[mainDishType], mainDish.Price);
+            Assert.AreEqual(quantity, mainDish.Quantity);
+            Assert.AreEqual(1, mainDish.SecondRank);
+            Assert.AreEqual(DishRanks.MainDishRanks[mainDishType], mainDish.ThirdRank);
+        }
+
+        [TestMethod]
+        public void InitializeMainDish_Schnitzel()
+        {
+            string name = "Schnitzel";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eMainDish mainDishType = eMainDish.Schnitzel;
+            MainDish mainDish = InitializeDishes.InitializeMainDish(name, quantity, dinerSerialNumber, mainDishType);
+
+            Assert.AreEqual(mainDishType, mainDish.MainDishType);
+            Assert.AreEqual(dinerSerialNumber, mainDish.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.MainDish], mainDish.MainRank);
+            Assert.AreEqual(name, mainDish.Name);
+            Assert.AreEqual(DishPrices.MainDishPrices[mainDishType], mainDish.Price);
+            Assert.AreEqual(quantity, mainDish.Quantity);
+            Assert.AreEqual(1, mainDish.SecondRank);
+            Assert.AreEqual(DishRanks.MainDishRanks[mainDishType], mainDish.ThirdRank);
+        }
+
+        [TestMethod]
+        public void InitializeMainDish_SpringChicken()
+        {
+            string name = "Spring Chicken";
+            int quantity = 2;
+            int dinerSerialNumber = 1;
+            eMainDish mainDishType = eMainDish.SpringChicken;
+            MainDish mainDish = InitializeDishes.InitializeMainDish(name, quantity, dinerSerialNumber, mainDishType);
+
+            Assert.AreEqual(mainDishType, mainDish.MainDishType);
+            Assert.AreEqual(dinerSerialNumber, mainDish.DinerSerialNumber);
+            Assert.AreEqual(DishRanks.DishesRank[eDishYypes.MainDish], mainDish.MainRank);
+            Assert.AreEqual(name, mainDish.Name);
+            Assert.AreEqual(DishPrices.MainDishPrices[mainDishType], mainDish.Price);
+            Assert.AreEqual(quantity, mainDish.Quantity);
+            Assert.AreEqual(1, mainDish.SecondRank);
+            Assert.AreEqual(DishRanks.MainDishRanks[mainDishType], mainDish.ThirdRank);
+        }
+    }
+}
